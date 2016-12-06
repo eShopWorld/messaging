5a7bb15b448a4917fef719e24ca0901cf6e572bf
djfr <davidferreirarodrigues@gmail.com>
2016-12-06 10:39:56 +0000

    Added the remaining comments to all the public and internal API endpoints on the package

--- src/DevOpsFlex.Messaging/ErrorQueue.cs
+++ src/DevOpsFlex.Messaging/ErrorQueue.cs
@@ -5,18 +5,32 @@
     using JetBrains.Annotations;
     using Microsoft.ServiceBus.Messaging;
 
+    /// <summary>
+    /// This is a special queue used to error messages that have know detailed problems during
+    /// handling.
+    /// </summary>
     internal class ErrorQueue : IDisposable
     {
         internal const string ErrorQueueName = "error";
         internal readonly QueueClient QueueClient;
         private readonly IDisposable _errorSub;
 
+        /// <summary>
+        /// Initializes an instance of <see cref="ErrorQueue"/>
+        /// </summary>
+        /// <param name="connectionString">The Azure Service Bus connection string.</param>
+        /// <param name="messagesIn">The <see cref="IObservable{IMessage}"/> used to stream messages that are to be sent to the error queue.</param>
         internal ErrorQueue([NotNull]string connectionString, [NotNull]IObservable<IMessage> messagesIn)
         {
             QueueClient = QueueCllientExtensions.CreateIfNotExists(connectionString, "error").Result; // unwrapp
             _errorSub = messagesIn.Subscribe(async m => await SendToError(m));
         }
 
+        /// <summary>
+        /// Sends a message to the error queue. It's a very simple entry to QueueClient send.
+        /// </summary>
+        /// <param name="message">The <see cref="IMessage"/> that we want to send to the error queue.</param>
+        /// <returns>The async <see cref="Task"/> wrapper.</returns>
         internal async Task SendToError(IMessage message)
         {
             await QueueClient.SendAsync(new BrokeredMessage(message));
--- src/DevOpsFlex.Messaging/MessageQueue.cs
+++ src/DevOpsFlex.Messaging/MessageQueue.cs
@@ -7,6 +7,10 @@
     using JetBrains.Annotations;
     using Microsoft.ServiceBus.Messaging;
 
+    /// <summary>
+    /// Generics based message queue router from <see cref="IObservable{IMessage}"/> through to the <see cref="QueueClient"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the message that we are routing.</typeparam>
     internal sealed class MessageQueue<T> : MessageQueue
         where T : IMessage
     {
@@ -15,6 +19,12 @@
 
         internal Timer ReadTimer;
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="MessageQueue{T}"/>.
+        /// </summary>
+        /// <param name="connectionString">The Azure Service Bus connection string.</param>
+        /// <param name="messagesIn">The <see cref="IObserver{IMessage}"/> used to push received messages into the pipeline.</param>
+        /// <param name="messagesOut">The <see cref="IObservable{IMessage}"/> used to take messages that are sent from the pipeline.</param>
         public MessageQueue([NotNull]string connectionString, [NotNull]IObserver<IMessage> messagesIn, [NotNull]IObservable<IMessage> messagesOut)
             : base(connectionString, typeof(T))
         {
@@ -23,6 +33,9 @@
             OutSubscription = messagesOut.Subscribe(async m => await Send(m));
         }
 
+        /// <summary>
+        /// Starts pooling the queue in order to read messages in Peek Lock mode.
+        /// </summary>
         internal void StartReading()
         {
             if (ReadTimer != null) return;
@@ -34,17 +47,30 @@
                 TimeSpan.FromSeconds(1)); // TODO: CREATE A DYNAMIC POOLING HEURISTIC
         }
 
+        /// <summary>
+        /// Stops pooling the queue for reading messages.
+        /// </summary>
         internal void StopReading()
         {
             ReadTimer.Dispose();
             ReadTimer = null;
         }
 
+        /// <summary>
+        /// Sends a single message.
+        /// </summary>
+        /// <param name="message">The message we want to send.</param>
+        /// <returns>The async <see cref="Task"/> wrapper.</returns>
         internal async Task Send([NotNull]IMessage message)
         {
             await QueueClient.SendAsync(new BrokeredMessage(message));
         }
 
+        /// <summary>
+        /// [BATCHED] Read message call back.
+        /// </summary>
+        /// <param name="_">[Ignored]</param>
+        /// <returns>The async <see cref="Task"/> wrapper.</returns>
         internal async Task Read([CanBeNull]object _)
         {
             var messages = await QueueClient.ReceiveBatchAsync(BatchSize);
@@ -70,6 +96,9 @@
         }
     }
 
+    /// <summary>
+    /// Non generic message queue router from <see cref="IObservable{IMessage}"/> through to the <see cref="QueueClient"/>.
+    /// </summary>
     internal class MessageQueue : IDisposable
     {
         internal const int LockInSeconds = 60;
@@ -84,6 +113,11 @@
 
         internal int BatchSize = 10;
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="MessageQueue"/>.
+        /// </summary>
+        /// <param name="connectionString">The Azure Service Bus connection string.</param>
+        /// <param name="messageType">The fully strongly typed <see cref="Type"/> of the message we want to create the queue for.</param>
         internal MessageQueue([NotNull]string connectionString, [NotNull]Type messageType)
         {
             if (messageType.FullName.Length > 260) // SB quota: Entity path max length
@@ -101,6 +135,12 @@ I suggest you reduce the size of the namespace '{messageType.Namespace}'.");
             QueueClient = QueueCllientExtensions.CreateIfNotExists(connectionString, messageType.FullName).Result; // unwrapp
         }
 
+        /// <summary>
+        /// Releases a message from the Queue by releasing all the specific message resources like lock
+        /// renewal timers.
+        /// This is called by all the methods that terminate the life of a message like COMPLETE, ABANDON and ERROR.
+        /// </summary>
+        /// <param name="message">The message that we want to release.</param>
         internal static void Release(IMessage message)
         {
             lock (Gate)

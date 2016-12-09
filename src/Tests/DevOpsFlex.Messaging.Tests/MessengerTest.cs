638049b1eee79fe5d8ef36a8896f6d1b68e52ecf
djfr <davidferreirarodrigues@gmail.com>
2016-12-09 09:17:54 +0000

    Added a memory profiler test for send

--- src/Tests/DevOpsFlex.Messaging.Tests/MessengerTest.cs
+++ src/Tests/DevOpsFlex.Messaging.Tests/MessengerTest.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using DevOpsFlex.Messaging;
 using DevOpsFlex.Messaging.Tests;
 using FluentAssertions;
+using JetBrains.dotMemoryUnit;
 using Microsoft.ServiceBus;
 using Microsoft.ServiceBus.Messaging;
 using Microsoft.VisualStudio.Profiler;
@@ -136,16 +136,33 @@ public class MessengerTest
     [TestClass]
     public class Profiler
     {
-        [TestMethod, TestCategory("Profiler")]
-        public async Task Profile_SendBurst_100_TestMessage()
+        [TestMethod, TestCategory("CPUProfiler")]
+        public async Task CPUProfile_SendBurst_100_TestMessage()
         {
             await SendBurst(100);
         }
 
-        [TestMethod, TestCategory("Profiler")]
-        public async Task Profile_SendBurst_1000_TestMessage()
+        [TestMethod, TestCategory("CPUProfiler")]
+        public async Task CPUProfile_SendBurst_1000_TestMessage()
         {
             await SendBurst(1000);
+        }
+
+        /// <remarks>
+        /// This is a dotMemory test, so it requires R# + dotMemory and it needs to be run
+        /// through the R# test runner using the specific "run under dotMemory Unit" option or it will throw.
+        /// </remarks>
+        [TestMethod, TestCategory("MemoryProfiler")]
+        public void MemoryProfile_SendBurst_100_NoMessageLeaks()
+        {
+            const int count = 100;
+            SendBurstMemoryIsolation(count).Wait();
+            GC.Collect(2, GCCollectionMode.Forced);
+
+            dotMemory.Check(memory => memory.GetObjects(o => o.Type == typeof(TestMessage)).ObjectsCount.Should().Be(0));
+
+            // can't guarantee full release inside the ASB SDK, just need to ensure that at the end of the test retention is low (below 5%)
+            dotMemory.Check(memory => memory.GetObjects(o => o.Type == typeof(BrokeredMessage)).ObjectsCount.Should().BeLessThan((int) (count * 0.05)));
         }
 
         private static async Task SendBurst(int count)
@@ -171,9 +188,30 @@ public class MessengerTest
                 }
             }
         }
+
+        private static async Task SendBurstMemoryIsolation(int count)
+        {
+            await NamespaceManager.CreateFromConnectionString(NamespaceHelper.GetConnectionString()).ScorchNamespace();
+
+            var messages = new List<TestMessage>();
+            for (var i = 0; i < count; i++)
+            {
+                messages.Add(new TestMessage());
+            }
+
+            using (IMessenger msn = new Messenger(NamespaceHelper.GetConnectionString()))
+            {
+                DataCollection.StartProfile(ProfileLevel.Global, DataCollection.CurrentId);
+                foreach (var message in messages)
+                {
+                    await msn.Send(message);
+                }
+            }
+
+            messages.Clear(); // force List release
+        }
     }
 }
-
 
 public static class MessengerTestExtensions
 {

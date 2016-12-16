fcee7e3770c8c31425495e5d60766e61f0bfdceb
djfr <davidferreirarodrigues@gmail.com>
2016-12-16 11:00:39 +0000

    Added XML docs to the package

--- src/DevOpsFlex.Messaging/IMessenger.cs
+++ src/DevOpsFlex.Messaging/IMessenger.cs
@@ -4,6 +4,9 @@
     using System.Threading.Tasks;
     using JetBrains.Annotations;
 
+    /// <summary>
+    /// Contract that provides a simple way to send messages.
+    /// </summary>
     public interface ISendMessages : IDisposable
     {
         /// <summary>
@@ -14,18 +17,24 @@
         Task Send<T>([NotNull] T message) where T : IMessage;
     }
 
+    /// <summary>
+    /// Contract that provides a simple way to send and receive messages through callbacks.
+    /// </summary>
     public interface IMessenger : ISendMessages
     {
         /// <summary>
         /// Sets up a call back for receiving any message of type <typeparamref name="T"/>.
-        /// If you try to setup more then one callback to the same message type <see cref="T"/> you'll get an <see cref="InvalidOperationException"/>.
+        /// If you try to setup more then one callback to the same message type <typeparamref name="T"/> you'll get an <see cref="InvalidOperationException"/>.
         /// </summary>
         /// <typeparam name="T">The type of the message that we are subscribing to receiving.</typeparam>
         /// <param name="callback">The <see cref="Action{T}"/> delegate that will be called for each message received.</param>
-        /// <exception cref="InvalidOperationException">Thrown when you attempt to setup multiple callbacks against the same <see cref="T"/> parameter.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when you attempt to setup multiple callbacks against the same <typeparamref name="T"/> parameter.</exception>
         void Receive<T>([NotNull] Action<T> callback) where T : IMessage;
     }
 
+    /// <summary>
+    /// Contract that exposes a reactive way to receive and send messages.
+    /// </summary>
     public interface IReactiveMessenger : ISendMessages
     {
         /// <summary>
--- src/DevOpsFlex.Messaging/Messenger.cs
+++ src/DevOpsFlex.Messaging/Messenger.cs
@@ -59,11 +59,11 @@
 
         /// <summary>
         /// Sets up a call back for receiving any message of type <typeparamref name="T"/>.
-        /// If you try to setup more then one callback to the same message type <see cref="T"/> you'll get an <see cref="InvalidOperationException"/>.
+        /// If you try to setup more then one callback to the same message type <typeparamref name="T"/> you'll get an <see cref="InvalidOperationException"/>.
         /// </summary>
         /// <typeparam name="T">The type of the message that we are subscribing to receiving.</typeparam>
         /// <param name="callback">The <see cref="Action{T}"/> delegate that will be called for each message received.</param>
-        /// <exception cref="InvalidOperationException">Thrown when you attempt to setup multiple callbacks against the same <see cref="T"/> parameter.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when you attempt to setup multiple callbacks against the same <typeparamref name="T"/> parameter.</exception>
         public void Receive<T>(Action<T> callback)
             where T : IMessage
         {

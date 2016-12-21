77ac59adcb661ba0b91c3cf46895dae9be8c7eb6
djfr <davidferreirarodrigues@gmail.com>
2016-12-21 10:17:06 +0000

    Added missing documentation to the Dictionary extensions

--- src/DevOpsFlex.Messaging/DictionaryExtensions.cs
+++ src/DevOpsFlex.Messaging/DictionaryExtensions.cs
@@ -4,16 +4,17 @@
     using System.Collections.Generic;
 
     /// <summary>
-    /// 
+    /// Contains extensions to <see cref="System.Collections.IDictionary"/>.
     /// </summary>
     public static class DictionaryExtensions
     {
         /// <summary>
-        /// 
+        /// Releases a <see cref="IDictionary{TKey,TValue}"/> where <typeparamref name="TValue"/> implements <see cref="IDisposable"/> by calling
+        /// Dispose() on all it's values and then clearing it.
         /// </summary>
-        /// <param name="source"></param>
-        /// <typeparam name="TKey"></typeparam>
-        /// <typeparam name="TValue"></typeparam>
+        /// <param name="source">The source <see cref="IDictionary{TKey,TValue}"/> that we want to release.</param>
+        /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+        /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
         public static void Release<TKey, TValue>(this IDictionary<TKey, TValue> source)
             where TValue : IDisposable
         {

5b3e4ba118833f2d7a709e55d6a5e77ae7ff3a7c
djfr <davidferreirarodrigues@gmail.com>
2018-09-05 16:50:27 +0100

    Supressed the generic type discards warnings in tests

---
+++ src/Tests/Eshopworld.Messaging.Tests/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage(
+    category: "Usage",
+    checkId: "xUnit1026:Theory methods should use all of their parameters",
+    Justification = "They are discards to inject member data as a generics type parameter",
+    Scope = "type",
+    Target = "MessengerTest")]

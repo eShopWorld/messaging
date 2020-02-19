8e9cf996b36b1558b8052c4e2f1e78257259164b
David Guerin <david@developershelf.com>
2020-02-19 18:54:52 +0000

    undo skip for lock test

--- src/Tests/Eshopworld.Messaging.Tests/MessengerQueueTest.cs
+++ src/Tests/Eshopworld.Messaging.Tests/MessengerQueueTest.cs
@@ -111,7 +111,7 @@ public class MessengerQueueTest
         }
     }
 
-    [Fact(Skip = "delay is annoying"), IsLayer1]
+    [Fact, IsLayer1]
     public async Task Test_LockMessage_ForTwoMinutes()
     {
         await ServiceBusFixture.ServiceBusNamespace.ScorchNamespace();
